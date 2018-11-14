55a949bc5f2faa9798bd44d41e5092132b4c4d5d
CEP <galcera@hotmail.com>
2018-11-14 10:48:03 +0100

    Cambios puntuales
    fassfds

---
+++ adami_project/forms/rutas.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Recordad ordenar por orden alfabético!!
+ */
+
+$rutas = array(
+    "bootstrap_css"=>"../assets/libs/bootstrap/css/bootstrap.min.css",
+    "bootstrap_jquery"=>"../assets/libs/bootstrap/js/jquery-3.3.1.min.js",
+    "bootstrap_js"=>"../assets/libs/bootstrap/js/bootstrap.min.js",
+    "bootstrap_jpopper"=>"../assets/libs/bootstrap/js/popper.min.js",
+    "index" => "../index.php",
+    "logo" => "../assets/media/img/Logov2.png",
+    "logo_ico" => "",
+);
+?>
